60d1f6b90e3a4cc648148cb0a9c34b8a7de75b03
Kunal Patil <patil.kunal919@gmail.com>
2022-05-05 02:41:34 -0700

    Setup character and enemy classes

--- jump.cpp
+++ jump.cpp
@@ -1,1 +1,73 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp >
+#include "jump.h"
+
+
+enemy::enemy() : clock(), body() { //first default construct clock and body
+
+	constexpr float side_length = 150 / 6; // the side length is the window height (150) over 6
+
+	body.setFillColor(sf::Color::Yellow); //make it yellow
+	body.setSize(sf::Vector2f(side_length, side_length)); //set the size appropriately
+
+	//x position is the length of the window (450)
+	//y position twice the height of the square
+	body.setPosition(sf::Vector2f(450, 2*side_length)); //set the position to be the rightmost of the screen, lying on the ground
+}
+
+void enemy::update_position(sf::Vector2f pos){
+	body.setPosition(pos);
+}
+
+float enemy::get_x_min() const{
+
+	//get the origin of the body (top left corner) and return the x part
+	const sf::Vector2f origin = body.getOrigin(); 
+	return origin.x;
+}
+
+float enemy::get_y_max() const {
+
+	//get the origin of the body (top left corner) and return the y part 
+	const sf::Vector2f origin = body.getOrigin();
+	return origin.y;
+}
+
+character::character() : init_pos(0.f, float(150-150/6)), last_jump(), top(), bottom() { //construct the init post to be x position of 0 and y position to be window height minus ground height, default construct last jump, top and bottom
+
+
+	//set the correct properties of the shapes
+	constexpr float window_height = 150; 
+	constexpr float square_length = window_height / 6; // square length is one sixth of the window height
+	constexpr float rectangle_length = square_length * 3; // rectangle length is three times the square length
+	constexpr float bottom_x_pos = 0; //the intial x position of the bottom part is 0
+	constexpr float bottom_y_pos = window_height - 2*square_length; //the inital y position of the bottom part is the window height minus twice the square length
+	constexpr float top_y_pos = window_height - 3 * square_length; //the initial y position of the top part is the window height minus three times the square legnth
+	constexpr float top_x_pos = square_length; //the intial x positon of the top part is the square length
+
+	bottom.setSize(sf::Vector2f(rectangle_length, square_length));
+	bottom.setPosition(sf::Vector2f(bottom_x_pos, bottom_y_pos));
+	bottom.setFillColor(sf::Color::Red);
+
+	top.setSize(sf::Vector2f(square_length, square_length));
+	top.setPosition(sf::Vector2f(top_x_pos, top_y_pos));
+	top.setFillColor(sf::Color::Red);
+
+	//initialize the sounds 
+	sf::SoundBuffer hit_buffer;
+	if (!hit_buffer.loadFromFile("Resources/hit.wav")) {//if we are unable to load the sound file, throw an error
+		throw std::logic_error("Could not open file hit.wav");
+	}
+	hit = sf::Sound(hit_buffer);
+
+	sf::SoundBuffer jump_buffer;
+	if (!jump_buffer.loadFromFile("Resources/jump.wav")) {//if we are unable to load the sound file, throw an error
+		throw std::logic_error("Could not open file jump.wav");
+	}
+	jump = sf::Sound(jump_buffer); 
+}
+
+
+
+
+
--- jump.h
+++ jump.h
@@ -1,5 +1,77 @@
 #ifndef _JUMP_
 #define _JUMP_
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+
+/**
+@class enemy represents the blocks that come at the player in the game
+*/
+class enemy : public sf::Shape{
+
+private:
+
+	static constexpr int speed = 200; // speed of the enemy in pixels/second
+	sf::Clock clock; //clock to keep track of position of enemy
+	sf::RectangleShape body; //body of the enemy
+
+
+public:
+
+	/**
+	default constructor for enemy 
+	*/
+	enemy(); 
+
+	/**
+	simple function to update the position of the enemy to be called upon by Game
+	@param pos the new position of enemy
+	*/
+	void update_position(sf::Vector2f pos);
+
+	/**
+	simple function to find the leftmost position of the enemy
+	@return the smallest x position of the enemy
+	*/
+	float get_x_min() const;
+
+	/**
+	simple function to find the topmost position of the enemy
+	@return the largest y position of the enemy
+	*/
+	float get_y_max() const;
+
+};
+
+
+/**
+@class character that represents the object that the user controls
+*/
+class character {
+
+private:
+
+	//some static variables for calculating jump height
+	static constexpr float T1 = 1;
+	static constexpr float T2 = 1.1;
+
+	const sf::Vector2f init_pos; //static variable to keep track of the intial bottom left position (x pos is 0, y pos is the height of the window)
+	sf::Clock last_jump; //clock to keep track of time since the last jump
+	sf::RectangleShape top; //square part of the character
+	sf::RectangleShape bottom; //rectangle part of the character
+	sf::Sound hit; //sound to make when hit
+	sf::Sound jump; //sound to make when jump
+
+public:
+
+	/**
+	default constructor for character class
+	*/
+	character(); 
+
+
+
+};
 
 
 
